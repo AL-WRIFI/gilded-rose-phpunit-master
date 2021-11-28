0bbbbe58b82e250396acb3b770516625945a625f
ALWRIFI <alwrifisoft@gmail.com>
2021-11-28 06:35:45 +0300

    Remove all overlapping -if-

--- src/GildedRose/GildedRose.php
+++ src/GildedRose/GildedRose.php
@@ -42,13 +42,11 @@ class GildedRose
            }elseif($this->name == "Backstage passes to a TAFKAL80ETC concert"){
             
             $this->quality +=1;
-             
-            
+               
             if($this->sellIn <=10){
                
                 $this->quality +=1;  
              }
-
 
             if($this->sellIn <=5){
                
@@ -69,58 +67,22 @@ class GildedRose
              $this->sellIn -=1;
              
              return;
+
            }elseif($this->name == "Sulfuras, Hand of Ragnaros"){
-               return;
+               
+            return;
+
+           }else{
+
+            $this->quality -=1;          
+            $this->sellIn -=1;
+            
+            if($this->sellIn <=0){
+               
+                $this->quality -=1;  
+             }
+              
+             return;
            }
-
-
-
-        if ($this->name != "Aged Brie" && $this->name != "Backstage passes to a TAFKAL80ETC concert") {
-            if ($this->quality > 0) {
-                if ($this->name != "Sulfuras, Hand of Ragnaros") {
-                    $this->quality = $this->quality - 1;
-                }
-            }
-        } else {
-            if ($this->quality < 50) {
-                $this->quality = $this->quality + 1;
-
-                if ($this->name == "Backstage passes to a TAFKAL80ETC concert") {
-                    if ($this->sellIn < 11) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
-                    }
-
-                    if ($this->sellIn < 6) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if ($this->name != "Sulfuras, Hand of Ragnaros") {
-            $this->sellIn = $this->sellIn - 1;
-        }
-
-        if ($this->sellIn < 0) {
-            if ($this->name != "Aged Brie") {
-                if ($this->name != "Backstage passes to a TAFKAL80ETC concert") {
-                    if ($this->quality > 0) {
-                        if ($this->name != "Sulfuras, Hand of Ragnaros") {
-                            $this->quality = $this->quality - 1;
-                        }
-                    }
-                } else {
-                    $this->quality = $this->quality - $this->quality;
-                }
-            } else {
-                if ($this->quality < 50) {
-                    $this->quality = $this->quality + 1;
-                }
-            }
-        }
     }
 }
